3782c17473dcc41d57375f78bc533d43283ba044
killi1812 <cvok.fran@gmail.com>
2024-06-05 12:56:12 +0200

    Settings scalable

--- WpfGui/SettingsWinfow.xaml.cs
+++ WpfGui/SettingsWinfow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FootballData.ProjectSettings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,11 @@ namespace WpfGui
         public SettingsWinfow()
         {
             InitializeComponent();
+        }
+        private void loadSettings()
+        {
+            Settings settings = Settings.GetSettings(); 
+            
         }
     }
 }
